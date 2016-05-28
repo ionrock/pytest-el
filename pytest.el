19d8529551f1c4f3e3eb3162194d246746c620e3
Olivier Verdier <olivier.verdier@gmail.com>
2016-05-28 13:54:41 +0200

    fix spaces in path and running all tests
    The resulting string became empty, which caused errors
    The strategy here is to execute py.test on "." instead of
    an empty list

--- pytest.el
+++ pytest.el
@@ -119,7 +119,7 @@ Optional argument FLAGS py.test command line flags."
                     (let ((testpath (if (listp tests) (car tests) tests)))
                       (pytest-find-project-root (file-name-directory testpath)))
                   (pytest-find-project-root)))
-         (tests (cond ((not tests) "")
+         (tests (cond ((not tests) (list "."))
                       ((listp tests) tests)
                       ((stringp tests) (split-string tests))))
          (_ (mapc 'pytest-check-test-file tests))
